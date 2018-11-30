03199e5258aff28809abc1eca22c17d20394cadf
Fikri Adriansa <fikriadriansa21@gmail.com>
2018-11-30 11:24:54 +0700

    commit show favorite data list

--- app/src/main/java/com/fikriadriansa/footballschedule/activity/FavoriteDetailActivity.kt
+++ app/src/main/java/com/fikriadriansa/footballschedule/activity/FavoriteDetailActivity.kt
@@ -10,23 +10,18 @@ import android.widget.Toast
 import com.fikriadriansa.footballschedule.R
 import com.fikriadriansa.footballschedule.api.ApiRepository
 import com.fikriadriansa.footballschedule.db.database
-import com.fikriadriansa.footballschedule.model.Event
 import com.fikriadriansa.footballschedule.model.Favorite
 import com.fikriadriansa.footballschedule.model.TeamDetail
-import com.fikriadriansa.footballschedule.presenter.MainPresenter
 import com.fikriadriansa.footballschedule.presenter.TeamDetailPresenter
-import com.fikriadriansa.footballschedule.view.MainView
 import com.fikriadriansa.footballschedule.view.TeamDetailView
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_event_detail.*
 import org.jetbrains.anko.db.delete
 
-class FavoriteDetailActivity : AppCompatActivity(),MainView,TeamDetailView {
+class FavoriteDetailActivity : AppCompatActivity(),TeamDetailView {
     private var detailTeam: MutableList<TeamDetail> = mutableListOf()
-    private var eventList: MutableList<Event> = mutableListOf()
     private lateinit var detailPresenter: TeamDetailPresenter
-    private lateinit var mainPresenter: MainPresenter
     private var menuItem: Menu? = null
 
 
@@ -42,27 +37,6 @@ class FavoriteDetailActivity : AppCompatActivity(),MainView,TeamDetailView {
 
     }
 
-    override fun showListMatch(data: List<Event>) {
-//        eventList.clear()
-//        eventList.addAll(data)
-//        tv_date_detail.text = data[0].dateEvent
-//        tv_score_home_detail.text = data[0].intHomeScore
-//        tv_goals_detail_home.text = data[0].strHomeGoalDetails.toString().replace(";","\n")
-//        tv_home_gk.text = data[0].strHomeLineupGoalkeeper.toString().replace(";","\n")
-//        tv_home_def.text = data[0].strHomeLineupDefense.toString().replace(";","\n")
-//        tv_home_mid.text = data[0].strHomeLineupMidfield.toString().replace(";","\n")
-//        tv_home_fwd.text = data[0].strHomeLineupForward.toString().replace(";","\n")
-//        tv_home_sub.text = data[0].strHomeLineupSubstitutes.toString().replace(";","\n")
-//
-//
-//        tv_score_away_detail.text = data[0].intAwayScore
-//        tv_goals_detail_away.text = data[0].strAwayGoalDetails.toString().replace(";","\n")
-//        tv_away_gk.text = data[0].strAwayLineupGoalkeeper.toString().replace(";","\n")
-//        tv_away_def.text = data[0].strAwayLineupDefense.toString().replace(";","\n")
-//        tv_away_mid.text = data[0].strAwayLineupMidfield.toString().replace(";","\n")
-//        tv_away_fwd.text = data[0].strAwayLineupForward.toString().replace(";","\n")
-//        tv_away_sub.text = data[0].strAwayLineupSubstitutes.toString().replace(";","\n")
-    }
 
     override fun showDetailHomeMatch(data: List<TeamDetail>) {
         detailTeam.clear()
@@ -108,10 +82,6 @@ class FavoriteDetailActivity : AppCompatActivity(),MainView,TeamDetailView {
         detailPresenter = TeamDetailPresenter(this, request, gson)
         detailPresenter.getDetailTeamHome(getDataFav.teamHomeId)
         detailPresenter.getDetailTeamAway(getDataFav.teamAwayId)
-
-//        mainPresenter = MainPresenter(this,request,gson)
-//        mainPresenter.getListNextMatch()
-//        mainPresenter.getListLastMatch()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
